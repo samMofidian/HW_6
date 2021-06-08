468307153f2877fe4579619bb0f35438a0820787
samMofidian <m.a.mofidian@hotmail.co.uk>
2021-06-09 00:53:06 +0430

    complete class Address

--- Address.cpp
+++ Address.cpp
@@ -3,3 +3,61 @@
 //
 
 #include "Address.h"
+
+#include <iostream>
+
+using namespace std;
+
+Address::Address(const std::string &country, const std::string &city, const std::string &street) :
+    country(country), city(city), street(street) {}
+
+const std::string &Address::getCountry() const {
+
+    return country;
+}
+
+const std::string &Address::getCity() const {
+
+    return city;
+}
+
+const std::string &Address::getStreet() const {
+
+    return street;
+}
+
+void Address::setCountry(const std::string &country) {
+
+    Address::country = country;
+}
+
+void Address::setCity(const std::string &city) {
+
+    Address::city = city;
+}
+
+void Address::setStreet(const std::string &street) {
+
+    Address::street = street;
+}
+
+ostream& operator <<(ostream& o, const Address& a) {
+
+    o << "country: " << a.getCountry() << ", city: " << a.getCity() << ", street: " << a.getStreet();
+    return o;
+}
+
+istream& operator >>(istream& i, Address& a) {
+
+    string country1, city1, street1;
+    cout << "Enter country: ";
+    i >> country1;
+    cout << "Enter city: ";
+    i >> city1;
+    cout << "Enter street: ";
+    i >> street1;
+    a.setCountry(country1);
+    a.setCity(city1);
+    a.setStreet(street1);
+    return i;
+}
--- Address.h
+++ Address.h
@@ -5,8 +5,38 @@
 #ifndef HW6_ADDRESS_H
 #define HW6_ADDRESS_H
 
+#include <iostream>
 
 class Address {
+
+private:
+
+    std::string country;
+    std::string city;
+    std::string street;
+
+public:
+
+    //constructoe
+    Address(const std::string &country, const std::string &city, const std::string &street);
+
+    //getter and setter
+    void setCountry(const std::string &country);
+
+    void setCity(const std::string &city);
+
+    void setStreet(const std::string &street);
+
+    const std::string &getCountry() const;
+
+    const std::string &getCity() const;
+
+    const std::string &getStreet() const;
+
+    //extraction and insertion operator
+    friend std::ostream& operator <<(std::ostream& o, const Address& a);
+
+    friend std::istream& operator >>(std::istream& i, Address& a);
 
 };
 
