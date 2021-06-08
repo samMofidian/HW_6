aea3e2fde5ea882d1a20e3fbb247867840f60c48
samMofidian <m.a.mofidian@hotmail.co.uk>
2021-06-09 03:27:59 +0430

    add class Employee

---
+++ Employee.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Ali on 6/9/2021.
+//
+
+#include "Employee.h"
+
+#include <iostream>
+
+using namespace std;
---
+++ Employee.h
@@ -0,0 +1,17 @@
+//
+// Created by Ali on 6/9/2021.
+//
+
+#ifndef HW6_EMPLOYEE_H
+#define HW6_EMPLOYEE_H
+
+#include "Person.h"]
+
+#include <iostream>
+
+class Employee : public Person {
+
+};
+
+
+#endif //HW6_EMPLOYEE_H
