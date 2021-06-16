9ed196962cd6b5f8878ac2e0d8f42fec3120694b
samMofidian <m.a.mofidian@hotmail.co.uk>
2021-06-16 22:23:38 +0430

    add other files

---
+++ cmake-build-debug/CMakeFiles/HW6.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/cygdrive/c/Users/Ali/Desktop/HW6/Address.cpp" "/cygdrive/c/Users/Ali/Desktop/HW6/cmake-build-debug/CMakeFiles/HW6.dir/Address.cpp.o"
+  "/cygdrive/c/Users/Ali/Desktop/HW6/Boss.cpp" "/cygdrive/c/Users/Ali/Desktop/HW6/cmake-build-debug/CMakeFiles/HW6.dir/Boss.cpp.o"
+  "/cygdrive/c/Users/Ali/Desktop/HW6/Company.cpp" "/cygdrive/c/Users/Ali/Desktop/HW6/cmake-build-debug/CMakeFiles/HW6.dir/Company.cpp.o"
+  "/cygdrive/c/Users/Ali/Desktop/HW6/Employee.cpp" "/cygdrive/c/Users/Ali/Desktop/HW6/cmake-build-debug/CMakeFiles/HW6.dir/Employee.cpp.o"
+  "/cygdrive/c/Users/Ali/Desktop/HW6/Person.cpp" "/cygdrive/c/Users/Ali/Desktop/HW6/cmake-build-debug/CMakeFiles/HW6.dir/Person.cpp.o"
+  "/cygdrive/c/Users/Ali/Desktop/HW6/main.cpp" "/cygdrive/c/Users/Ali/Desktop/HW6/cmake-build-debug/CMakeFiles/HW6.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/HW6.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HW6.dir/Address.cpp.o"
+  "CMakeFiles/HW6.dir/Boss.cpp.o"
+  "CMakeFiles/HW6.dir/Company.cpp.o"
+  "CMakeFiles/HW6.dir/Employee.cpp.o"
+  "CMakeFiles/HW6.dir/Person.cpp.o"
+  "CMakeFiles/HW6.dir/main.cpp.o"
+  "HW6.exe"
+  "HW6.pdb"
+  "libHW6.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HW6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
